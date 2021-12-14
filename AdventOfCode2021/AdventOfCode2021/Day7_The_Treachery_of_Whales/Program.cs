af59e66a3928a2fe69ed7433ec5a2f01dfad8f7c
anaskew29 <ana1003@jagmail.southalabama.edu>
2021-12-14 16:37:12 -0600

    Completed Day 7 - The Treachery of Whales
    Completed Day 7 parts 1 and 2 of Advent of Code 2021

---
+++ AdventOfCode2021/AdventOfCode2021/Day7_The_Treachery_of_Whales/Program.cs
@@ -0,0 +1,93 @@
+﻿using Day7_The_Treachery_of_Whales;
+using System;
+using System.IO;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] inputs;
+        List<int[]> subs = new List<int[]>();
+        using (StreamReader reader = new StreamReader("input.txt"))
+        //using (StreamReader reader = new StreamReader("example.txt"))
+        {
+            // input only has one line
+            inputs = (reader.ReadLine() ?? "").Split(',');
+        }
+
+        Console.WriteLine("Part1 = " + Part1(inputs));
+        Console.WriteLine("Part2 = " + Part2(inputs));
+    }
+
+    public static int Part1(string[] inputs)
+    {
+        int result = 0;
+        int maxPosition = 0;
+        int[] positions;
+        for (int i = 0; i < inputs.Length; i++)
+        {
+            int position = int.Parse(inputs[i]);
+            if (position > maxPosition)
+                maxPosition = position;
+        }
+        positions = new int[maxPosition+1];
+
+        // calculate the fuel cost for each position from each position in input
+        for (int pos = 0; pos < positions.Length; pos++)
+        {
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                int fuelCost = Math.Abs(int.Parse(inputs[i]) - pos);
+                positions[pos] += fuelCost;
+            }
+        }
+
+        result = positions[0];
+        foreach(int pos in positions)
+        {
+            if (pos < result)
+                result = pos;
+        }
+        return result;
+
+    }
+
+    public static int Part2(string[] inputs)
+    {
+        int result = 0;
+        int maxPosition = 0;
+        int[] positions;
+        for (int i = 0; i < inputs.Length; i++)
+        {
+            int position = int.Parse(inputs[i]);
+            if (position > maxPosition)
+                maxPosition = position;
+        }
+        positions = new int[maxPosition + 1];
+
+        // calculate the fuel cost for each position from each position in input
+        for (int pos = 0; pos < positions.Length; pos++)
+        {
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                int fuelCost = CalculateTriangularNumber(Math.Abs(int.Parse(inputs[i]) - pos));
+                positions[pos] += fuelCost;
+            }
+        }
+
+        result = positions[0];
+        foreach (int pos in positions)
+        {
+            if (pos < result)
+                result = pos;
+        }
+        return result;
+
+    }
+
+    public static int CalculateTriangularNumber(int val)
+    {
+        float n = float.Parse(val.ToString());
+        return Convert.ToInt32(n * ((n + 1) / 2));
+    }
+}
---
+++ AdventOfCode2021/AdventOfCode2021/Day7_The_Treachery_of_Whales/Submarine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7_The_Treachery_of_Whales
+{
+    internal class Submarine
+    {
+        public int Position { get; set; }
+        public int[] FuelCost { get; set; }
+
+        public Submarine(int position, int maxPosition)
+        {
+            FuelCost = new int[maxPosition+1];
+            for(int i = 0; i <= maxPosition; i++)
+            {
+                FuelCost[i] = Math.Abs(position - i);
+            }
+        }
+    }
+}
